b47839300eea85c066ae568c26a8881a0783f424
Chandra Tamang <ctamang@umd.edu>
2020-11-03 22:27:30 -0500

    i created a class, added two functions and docstring

---
+++ covid_info.py
@@ -0,0 +1,32 @@
+"""
+INST 326 FINAL PROJECT
+Group - 107
+Prof: Aric Bills
+
+Chandra Tamang
+Kristen Wagner
+Minsung Kim
+Diego Amores
+"""
+class covid:
+    """
+    class will have 8 different functions plaese list your function below.
+    """
+    # please add your function here
+
+
+
+
+
+
+    def reg_data(self, filename):
+        """
+        chandra
+        this function will find state and numbers of deaths, postive,
+        and negative in file and store it in variable that will be use to visualized these data.
+        """
+    def graph(self, x, y):
+        """
+        chandra
+        This function will do data visualization using matplotlib
+        """

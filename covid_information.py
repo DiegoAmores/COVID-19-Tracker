334cf77950f892c492d026fde7fe643b2ba954a0
Diego Amores <Diego.amores14@gmail.com>
2020-12-20 21:27:01 -0500

    Added Documentation for covid_information object class

--- covid_information.py
+++ covid_information.py
@@ -1,5 +1,7 @@
 from datetime import date
-#
+
+#Programmer: Diego Amores
+# COVID Report Object Class
 class CovidInfo:
     date = ""
     state = ""
@@ -13,25 +15,32 @@ class CovidInfo:
         self.fips = fips
         self.cases = cases
         self.deaths = deaths
-    
+        
+    #compares COVID reports.
     def __eq__(self, other):
         return (self.date, self.state) == (other.date, other.state)
     
+    #creates hashcode for COVID object based on date and state.
     def __hash__(self):
         return hash((self.date, self.state))
     
+    #gets the date of a COVID report.
     def get_date(self):
         return self.date
     
+    #gets the state of a COVID report.
     def get_state(self):
         return self.state
     
+    #gets the fips code of a state.
     def get_fips(self):
         return self.fips
     
+    #gets the number of cases of a COVID report.
     def get_cases(self):
         return self.cases
     
+    #gets the number of deaths of a COVID report.
     def get_deaths(self):
         return self.deaths
     
